a81275cd84a49a22deda2c0671b41ea8d384acab
Hussien Alomari <hussienalomari7@gmail.com>
2025-10-03 21:36:11 +0300

    Add files via upload

--- java.js
+++ java.js
@@ -43,3 +43,9 @@ if (video && soundIconWrapper) {
     }
   });
 }
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
